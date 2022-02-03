722dbb611d41637be0aa3927a3d6903299677f77
connorshort <connorshort42@gmail.com>
2022-02-03 16:29:38 -0500

    More testing and debugging, fixed string handling

--- LexerPrintTest.java
+++ LexerPrintTest.java
@@ -4,13 +4,16 @@ public class LexerPrintTest {
     public static void main(String[] args) throws LexicalException {
         String test= """
                 keyword
-                123
+                0023
                  "hi"
                 getRed
                 123.42
-                
+                string(BLACK) == true
+                "adsjf lkjal"
                 #this is a comment
-                boolean cheese 432abcd""";
+                boolean cheese 432abcd
+                "
+                """;
         Lexer testLexer=new Lexer(test);
         do{
             Token myToken=testLexer.next();

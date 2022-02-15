f1caf26b621865aa8357a3e8b1abc62069631f54
connorshort <connorshort42@gmail.com>
2022-02-15 17:02:00 -0500

    More setup

--- Parser.java
+++ Parser.java
@@ -5,13 +5,21 @@ import edu.ufl.cise.plc.IToken.SourceLocation;
 import edu.ufl.cise.plc.IParser;
 import edu.ufl.cise.plc.ast.ASTNode;
 
+import java.util.List;
+
 
 public class Parser implements IParser{
     String code;
+    List<IToken> tokens;
     public Parser(String input){
         this.code=input;
     }
     public ASTNode parse() throws PLCException{
+        //generate list of tokens using the lexer
+        ILexer lex=CompilerComponentFactory.getLexer(code);
+        do{
+            tokens.add(lex.next());
+        }while(tokens.get(tokens.size() - 1).getKind() != Kind.EOF);
 
     }
 }

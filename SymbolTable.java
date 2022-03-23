54e17da4507e04368d86fb1c67e5545b17ea7af6
connorshort <connorshort42@gmail.com>
2022-03-23 16:23:23 -0400

    Started project part 4

---
+++ SymbolTable.java
@@ -0,0 +1,21 @@
+package edu.ufl.cise.plc;
+
+import edu.ufl.cise.plc.ast.Declaration;
+import edu.ufl.cise.plc.ast.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+
+//TODO:  Implement a symbol table class that is appropriate for this language.
+    Map<String, Declaration> symbols = new HashMap<>();
+
+    public boolean add(String name, Declaration declaration){
+        return (symbols.putIfAbsent(name, declaration)==null);
+    }
+
+    public Declaration getDeclaration(String name){
+        return symbols.get(name);
+    }
+}
